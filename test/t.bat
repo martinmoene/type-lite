9af76d34929ba580c06c91ec2d81440f06d3e25f
Martin Moene <martin.moene@gmail.com>
2018-08-29 23:26:41 +0200

    Obviate need for -DNOMINMAX with MSVC (nonstd-lite issue 16)

--- test/t.bat
+++ test/t.bat
@@ -2,5 +2,5 @@
 @set std=%1
 @if not "%std%"=="" set std=-std:%std%
 set cppcorecheckinclude=%VCINSTALLDIR%/Auxiliary/VS/include
-cl -W3 -EHsc %std% -DNOMINMAX -I../include/nonstd -I"%cppcorecheckinclude%" type-main.t.cpp type.t.cpp && type-main.t.exe
+cl -W3 -EHsc %std% -I../include/nonstd -I"%cppcorecheckinclude%" type-main.t.cpp type.t.cpp && type-main.t.exe
 @endlocal
