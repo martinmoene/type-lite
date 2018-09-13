7109a2e07a977086d4cb36123cce04ae09e2eb5b
Martin Moene <martin.moene@gmail.com>
2018-09-13 12:09:00 +0200

    Add GNUC -Wconversion, -Wsign-conversion warnings

--- test/tg.bat
+++ test/tg.bat
@@ -1,6 +1,6 @@
 @setlocal
 @set std=%1
 @if "%std%"=="" set std=c++11
-g++ -std=%std% -O2 -Wall -Wextra -Wno-unused-parameter -I../include/nonstd -o type-main.t.exe type-main.t.cpp type.t.cpp && type-main.t.exe
+g++ -std=%std% -O2 -Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion -Wno-unused-parameter -I../include/nonstd -o type-main.t.exe type-main.t.cpp type.t.cpp && type-main.t.exe
 @endlocal
 
