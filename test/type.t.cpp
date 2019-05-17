9ed6a96637ffed746d9cda539bdad7e68d9e338f
Martin Moene <martin.moene@gmail.com>
2019-05-17 11:00:11 +0200

    Add expectations to tests

--- test/type.t.cpp
+++ test/type.t.cpp
@@ -33,11 +33,15 @@ CASE( "type: Disallows to default-construct a type thus defined (define type_CON
 CASE( "type: Allows to default-construct a type thus defined" )
 {
     type< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "type: Allows to construct a type from its underlying type" )
 {
     type< char, struct Tag > x('y');
+
+    EXPECT( x.get() == 'y' );
 }
 
 CASE( "boolean: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -50,11 +54,15 @@ CASE( "boolean: Disallows to default-construct a type thus defined (define type_
 CASE( "boolean: Allows to default-construct a type thus defined" )
 {
     nonstd::boolean< struct Tag > x;
+
+    EXPECT( x.get() == false );
 }
 
 CASE( "boolean: Allows to construct a type from its underlying type" )
 {
     nonstd::boolean< struct Tag > x( true );
+
+    EXPECT( x.get() == true );
 }
 
 CASE( "logical: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -67,11 +75,15 @@ CASE( "logical: Disallows to default-construct a type thus defined (define type_
 CASE( "logical: Allows to default-construct a type thus defined" )
 {
     logical< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "logical: Allows to construct a type from its underlying type" )
 {
     logical< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 CASE( "equality: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -84,11 +96,15 @@ CASE( "equality: Disallows to default-construct a type thus defined (define type
 CASE( "equality: Allows to default-construct a type thus defined" )
 {
     equality< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "equality: Allows to construct a type from its underlying type" )
 {
     equality< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 CASE( "bits: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -101,11 +117,15 @@ CASE( "bits: Disallows to default-construct a type thus defined (define type_CON
 CASE( "bits: Allows to default-construct a type thus defined" )
 {
     bits< unsigned int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "bits: Allows to construct a type from its underlying type" )
 {
     bits< unsigned int, struct Tag > x( 0x5a5a );
+
+    EXPECT( x.get() == 0x5a5a );
 }
 
 CASE( "ordered: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -118,11 +138,15 @@ CASE( "ordered: Disallows to default-construct a type thus defined (define type_
 CASE( "ordered: Allows to default-construct a type thus defined" )
 {
     ordered< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "ordered: Allows to construct a type from its underlying type" )
 {
     ordered< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 CASE( "numeric: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -135,11 +159,15 @@ CASE( "numeric: Disallows to default-construct a type thus defined (define type_
 CASE( "numeric: Allows to default-construct a type thus defined" )
 {
     numeric< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "numeric: Allows to construct a type from its underlying type" )
 {
     numeric< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 CASE( "quantity: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -152,11 +180,15 @@ CASE( "quantity: Disallows to default-construct a type thus defined (define type
 CASE( "quantity: Allows to default-construct a type thus defined" )
 {
     quantity< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "quantity: Allows to construct a type from its underlying type" )
 {
     quantity< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 CASE( "address: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -169,12 +201,20 @@ CASE( "address: Disallows to default-construct a type thus defined (define type_
 CASE( "address: Allows to default-construct a type thus defined" )
 {
     address< int *, std::ptrdiff_t, struct Tag > x;
+
+#if type_CPP11_OR_GREATER
+    EXPECT( x.get() == nullptr );
+#else
+    EXPECT( x.get() == NULL );
+#endif
 }
 
 CASE( "address: Allows to construct a type from its underlying type" )
 {
     int i;
     address< int *, std::ptrdiff_t, struct Tag > x( &i );
+
+    EXPECT( x.get() == &i );
 }
 
 CASE( "offset: Disallows to default-construct a type thus defined (define type_CONFIG_CONFIRMS_COMPILATION_ERRORS)" )
@@ -187,15 +227,19 @@ CASE( "offset: Disallows to default-construct a type thus defined (define type_C
 CASE( "offset: Allows to default-construct a type thus defined" )
 {
     offset< int, struct Tag > x;
+
+    EXPECT( x.get() == 0 );
 }
 
 CASE( "offset: Allows to construct a type from its underlying type" )
 {
     offset< int, struct Tag > x( 7 );
+
+    EXPECT( x.get() == 7 );
 }
 
 
-
+// -----------------------------------------------------------------------
 
 type_DEFINE_TYPE( Num, numeric, int )
 type_DEFINE_FUNCTION( Num, abs, std::abs )
